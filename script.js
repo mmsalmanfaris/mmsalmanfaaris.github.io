ae8d534ad3408ae45beec8855392381d9af1f6c5
mmsalmanfaris <mmsalmanfaaris01@gmail.com>
2024-03-17 21:30:16 +0530

    Pre Loader

---
+++ script.js
@@ -0,0 +1,11 @@
+// Pre Loader
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+  
+    loader.classList.add("loader--hidden");
+  
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  });
+  
